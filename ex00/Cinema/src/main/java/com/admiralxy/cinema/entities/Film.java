e957e394ffd169efd65670bf9b1cc95d05125db5
AdmiralXy <gameproblem80@gmail.com>
2022-08-25 02:26:00 +0300

    film columns nullable false

--- ex00/Cinema/src/main/java/com/admiralxy/cinema/entities/Film.java
+++ ex00/Cinema/src/main/java/com/admiralxy/cinema/entities/Film.java
@@ -11,16 +11,16 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class Film extends BaseEntity {
 
-    @Column(name = "title")
+    @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name = "release_year")
+    @Column(name = "release_year", nullable = false)
     private Integer releaseYear;
 
-    @Column(name = "age_restrictions")
+    @Column(name = "age_restrictions", nullable = false)
     private String ageRestrictions;
 
     @Lob
-    @Column(name = "description", length = 20000)
+    @Column(name = "description", length = 20000, nullable = false)
     private String description;
 }

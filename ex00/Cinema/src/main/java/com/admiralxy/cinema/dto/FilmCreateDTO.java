ec42b665a86efc46e8cb88bad2bf51ef2f00e293
AdmiralXy <gameproblem80@gmail.com>
2022-08-25 04:36:02 +0300

    remove mappable implements

--- ex00/Cinema/src/main/java/com/admiralxy/cinema/dto/FilmCreateDTO.java
+++ ex00/Cinema/src/main/java/com/admiralxy/cinema/dto/FilmCreateDTO.java
@@ -2,11 +2,10 @@ package com.admiralxy.cinema.dto;
 
 import lombok.Data;
 import com.admiralxy.cinema.entities.Film;
-import com.admiralxy.cinema.dto.interfaces.MappableDTO;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
-public class FilmCreateDTO implements MappableDTO<Film> {
+public class FilmCreateDTO {
 
     private String title;
     private Integer release_year;
@@ -14,16 +13,7 @@ public class FilmCreateDTO implements MappableDTO<Film> {
     private String description;
     private MultipartFile poster;
 
-    @Override
     public Film toEntity() {
         return new Film(this.title, this.release_year, this.age_restrictions, this.description);
-    }
-
-    @Override
-    public void fromEntity(Film film) {
-        this.title = film.getTitle();
-        this.release_year = film.getReleaseYear();
-        this.age_restrictions = film.getAgeRestrictions();
-        this.description = film.getDescription();
     }
 }
--- ex00/Cinema/src/main/java/com/admiralxy/cinema/dto/HallCreateDTO.java
+++ ex00/Cinema/src/main/java/com/admiralxy/cinema/dto/HallCreateDTO.java
@@ -1,24 +1,16 @@
 package com.admiralxy.cinema.dto;
 
 import lombok.Data;
-import com.admiralxy.cinema.dto.interfaces.MappableDTO;
 import com.admiralxy.cinema.entities.Hall;
 
 @Data
-public class HallCreateDTO implements MappableDTO<Hall> {
+public class HallCreateDTO {
 
     private Integer serial;
 
     private Integer seats;
 
-    @Override
     public Hall toEntity() {
         return new Hall(this.serial, this.seats);
-    }
-
-    @Override
-    public void fromEntity(Hall hall) {
-        this.serial = hall.getSerial();
-        this.seats = hall.getSeats();
     }
 }
